cd72d0dabc0d700eda09878c501862f38ea1be24
Insarchic <insareric@gmail.com>
2024-08-27 11:50:27 +0300

    Initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,73 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "job")
+    private String job;
+
+    @Column(name = "year")
+    private int yearOfBirth;
+
+    public User(String name, String job, int yearOfBirth) {
+        this.name = name;
+        this.job = job;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", name='" + name + '\'' +
+                ", job='" + job + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
+    }
+
+
+}
